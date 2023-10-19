a57130bb107fbcb5c5c9c0b0b5edb1a45dd54740
You Wei <thamyouwei@gmail.com>
2023-10-19 22:03:26 +0800

    Fix missing details getter test in CommandResult

--- src/test/java/seedu/letsgethired/logic/commands/CommandResultTest.java
+++ src/test/java/seedu/letsgethired/logic/commands/CommandResultTest.java
@@ -63,7 +63,7 @@ public class CommandResultTest {
         CommandResult commandResult = new CommandResult("feedback");
         String expected = CommandResult.class.getCanonicalName() + "{feedbackToUser="
                 + commandResult.getFeedbackToUser()
-                + ", detailsToUser="
+                + ", detailsToUser=" + commandResult.getDetailsToUser()
                 + ", showHelp=" + commandResult.isShowHelp()
                 + ", exit=" + commandResult.isExit() + "}";
         assertEquals(expected, commandResult.toString());
