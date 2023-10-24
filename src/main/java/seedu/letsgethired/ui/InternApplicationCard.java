41bb427cf3c2163e42d8522ff1d6d3205c833ce5
Timothy Putra Prasetio <timothypp02@gmail.com>
2023-10-24 21:56:30 +0800

    Add deadline fxml field to the UI

--- src/main/java/seedu/letsgethired/ui/InternApplicationCard.java
+++ src/main/java/seedu/letsgethired/ui/InternApplicationCard.java
@@ -41,6 +41,8 @@ public class InternApplicationCard extends UiPart<Region> {
     private Label note;
     @FXML
     private Label cycle;
+    @FXML
+    private Label deadline;
 
     /**
      * Creates a {@code InternApplicationCard} with the given {@code internApplication} and index to display.
@@ -59,6 +61,7 @@ public class InternApplicationCard extends UiPart<Region> {
         status.setText(internApplication.getStatus().value);
         note.setText(internApplication.getNote().value);
         cycle.setText(internApplication.getCycle().value);
+        deadline.setText(internApplication.getDeadline().value);
     }
 
     /**
