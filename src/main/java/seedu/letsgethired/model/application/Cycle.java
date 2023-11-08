fb69f7f90ab4db759f1a89b0a3cf70189f860e55
Timothy Putra Prasetio <timothypp02@gmail.com>
2023-11-08 17:22:18 +0800

    Remove hyphen from regex
    The Validation Regex will now follow the message constraints

--- src/main/java/seedu/letsgethired/model/application/Cycle.java
+++ src/main/java/seedu/letsgethired/model/application/Cycle.java
@@ -16,7 +16,7 @@ public class Cycle implements Comparable<Cycle> {
      * The first character of the cycle must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum}- ]*";
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
 
     public final String value;
 
